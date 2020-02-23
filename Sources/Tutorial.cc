7aa69e9ac702df68b6eea30dc0bf8ac869894c31
JeJo <email2jejo@gmail.com>
2020-02-23 09:59:42 +0100

    Adding a Version Number and Configured Header File

--- Sources/Tutorial.cc
+++ Sources/Tutorial.cc
@@ -1,7 +1,16 @@
 #include <iostream>
 
-int main()
+#include "TutorialConfig.h"
+
+int main(int argc, char *argv[])
 {
-	std::cout << "Hello world!\n";
+	if (argc < 2)
+	{
+    	// report version
+    	std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."
+              		<< Tutorial_VERSION_MINOR << std::endl;
+    	std::cout << "Usage: " << argv[0] << " number" << std::endl;
+    return 1;
+  	}
 	return 0;
 }
